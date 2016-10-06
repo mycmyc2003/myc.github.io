018a8c5a591d2ea6e457fe051d9a5232abd2e55c
mycmyc2003 <mycmyc2003@163.com>
2016-10-06 21:02:55 +0800

    HDU 1007
    still WA

--- code/1007.cpp
+++ code/1007.cpp
@@ -1,6 +1,8 @@
 /*
 http://blog.csdn.net/allenjy123/article/details/6627751
 这个题目其实就是求最近点对的距离。《算法导论》上有详细讲解，王晓东的书上也有代码。主要思想就是分治。先把n个点按x坐标排序，然后求左边n/2个和右边n/2个的最近距离，最后合并。
+另外参考这个：
+http://www.acmerblog.com/hdu-1007-Quoit-Design-1262.html
 */
 #include <iostream>
 #include <algorithm>
@@ -13,17 +15,22 @@ typedef struct _T_{
 }T;
 static T all[100000] = {0};
 #define MIN(a, b) (a)<(b)?(a):(b)
-
+static T tryit[100000] = {0};
+static int cnt = 0;
 int compare(const void *a, const void *b)
 {
 	return (((T*)a)->x - ((T*)b)->x);
 }
-float cal(int l, int r)
+inline float cal(int l, int r)
 {
 	double res1 = (all[r].x-all[l].x);
 	double res2 = (all[r].y-all[l].y);
 	return res1*res1+res2*res2;
 	//return (all[r].x-all[l].x)*(all[r].x-all[l].x) + (all[r].y-all[l].y)*(all[r].y-all[l].y);
+}
+inline float dis(float x1, float y1, float x2, float y2)
+{
+	return (x2-x1)*(x2-x1)+(y2-y1)*(y2-y1);
 }
 
 float go(int l, int r)
@@ -32,8 +39,20 @@ float go(int l, int r)
 	if(abs(r-l)==1) return cal(l, r);
 	float min_l = go(l, mid);
 	float min_r = go(mid, r);
-	
-	return (MIN(min_l, min_r));
+	float small = MIN(min_l, min_r);
+	float ref_x = (all[r].x - all[l].x)/2;
+	for(int kk = l; kk<r; kk++) 
+		if(dis(all[kk].x, ref_x, all[kk].y, all[kk].y) < small)  {tryit[cnt].x = all[kk].x; tryit[cnt].y = all[kk].y; cnt++;}
+	float smallest = 999999;
+	float temp = 0;
+	for(int i = 0; i<cnt; i++) {
+		for(int j = l; j<r; j++) {
+			temp = dis(tryit[i].x, all[j].x, tryit[i].y, all[j].y);
+			smallest = smallest > temp ? temp : smallest;
+		}
+	}
+	cnt = 0;
+	return (MIN(smallest, small));
 
 }
 
@@ -41,6 +60,8 @@ int main()
 {
 	int N = 0;
 	while(1) {
+		cnt = 0; //clear
+
 		cin>>N;
 		if(N == 0) break;
 		for(int i = 0; i<N; i++) 
